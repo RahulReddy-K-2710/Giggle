7780eaf394da22627d35d8b150e6fe383fc6e1cf
Roja <roja@3frameslab.com>
2022-12-04 08:40:31 +0530

    Initial commit

--- Giggle/ViewController/GiggleViewController.swift
+++ Giggle/ViewController/GiggleViewController.swift
@@ -11,23 +11,39 @@ class GiggleViewController: UIViewController {
     
     //MARK: Outlets.
     @IBOutlet weak var descTextView: UITextView!
-    @IBOutlet weak var tapButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var perviousButton: UIButton!
+    @IBOutlet weak var lightDarkSegentController: UISegmentedControl!
+    
     //MARK: Properties.
     let viewModel = GiggleViewModel()
+    var cacheJokes = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // set jokes.
+        
+        
         
     }
-    
+    // Create the nxt joke function.
     @IBAction func setUpJoke() {
         viewModel.getViewModel { Norries in
             DispatchQueue.main.async {
                 guard let joke = Norries?.joke else { return }
+                //store the jokes in jokesCache.
+                self.cacheJokes.append(joke)
                 self.descTextView.text = joke
             }
         }
+    }
+    // Create function for previous.
+    @IBAction func previousJoke() {
+        guard let recentJoke = cacheJokes.last else {
+            descTextView.text = "No Previous Jokes"
+            return
+        }
+        descTextView.text = recentJoke
+        cacheJokes.removeLast()
     }
     
 }
